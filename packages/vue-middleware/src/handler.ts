0f19d5e3c874b11ff59f66c80203e2a48266fdf6
Leachiml <34484044+leachiml@users.noreply.github.com>
2025-05-01 21:47:28 +0200

    refactor middleware handler to use recommended router navguard api instead of deprecated "next()" and improve type declaration

--- packages/vue-middleware/src/handler.ts
+++ packages/vue-middleware/src/handler.ts
@@ -1,178 +1,240 @@
-import type { App } from 'vue'
+import type { App } from "vue";
 import type {
   Router,
   RouteLocationNormalized,
   RouteMeta,
-  RouteLocationRaw,
-} from 'vue-router'
-import { Driver } from './drivers/driver'
+  RouteLocationNormalizedLoaded,
+  NavigationGuardReturn,
+  NavigationGuardWithThis,
+} from "vue-router";
+import { Driver } from "./drivers/driver";
 
 export interface MiddlewareContext {
-  app: App
-  router: Router
-  from: RouteLocationNormalized
-  to: RouteLocationNormalized
-  redirect: (to: RouteLocationRaw) => void
-  abort: () => Symbol | boolean
-  guard?: string | undefined
+  app: App;
+  router: Router;
+  from: RouteLocationNormalized;
+  to: RouteLocationNormalized;
+  guard?: string;
 }
 
+/**
+ * Registered Middleware List
+ */
+export interface Middlewares {
+  [key: string]: Middleware;
+}
+
+/**
+ * Registered Middleware
+ */
 export interface Middleware {
-  [key: string]: (ctx: MiddlewareContext) => void
+  (ctx: MiddlewareContext): NavigationGuardReturn;
 }
 
-type MiddlewareName = [string, string | undefined]
+/**
+ *  Middleware Name
+ *
+ * Consists of at least one part with an optional second part divided by a ":"
+ *
+ * E.g. "home:guest" or "home"
+ */
+type MiddlewareName = [string, string?];
 
+/**
+ *  Vue Middleware Plugin Options
+ */
 export interface Options {
-  middleware: Middleware
-  pageTitle: {
-    template: (name: string, meta: RouteMeta) => string
-  } | boolean
-  permissions: {
-    driver: new (app: App) => Driver
-  }
-  hooks: {
-    onBeforeEach?: (to: RouteLocationNormalized, from: RouteLocationNormalized) => void
-    onAfterEach?: (to: RouteLocationNormalized, from: RouteLocationNormalized) => void
-  }
+  /**
+   * Registered Middlewares that can be used by Routes
+   */
+  middleware: Middlewares;
+  pageTitle?:
+    | {
+        template: (name: string, meta: RouteMeta) => string;
+      }
+    | boolean;
+  permissions?: {
+    driver: new (app: App) => Driver;
+  };
+  hooks?: {
+    onBeforeEach?: (
+      to: RouteLocationNormalized,
+      from: RouteLocationNormalized
+    ) => void;
+    onAfterEach?: (
+      to: RouteLocationNormalized,
+      from: RouteLocationNormalized
+    ) => void;
+  };
 }
 
-const ABORT_KEY = Symbol('VMAbort')
-const abort = () => ABORT_KEY
+/**
+ * Main Vue Plugin handle
+ *
+ * @param app Vue App instance
+ * @param router Vue Router instance
+ * @param options Optional plugin Settings
+ */
+export function handler(app: App, router: Router, options: Options) {
+  //const { pageTitle, middleware, permissions, hooks } = options;
 
-export function handler(
-  app: App,
-  router: Router,
-  options: Partial<Options>
-) {
-  const {
-    pageTitle,
-    middleware = {},
-    permissions,
-    hooks,
-  } = options
-
-  let permissionsDriver = null
-  if (permissions?.driver) {
-    permissionsDriver = new permissions.driver(app)
+  let permissionsDriver: Driver | undefined;
+  if (options.permissions?.driver) {
+    permissionsDriver = new options.permissions.driver(app);
     if (!(permissionsDriver instanceof Driver)) {
       throw new Error(
         "The driver is not compatible with our base driver are you sure your're extending the base driver."
-      )
+      );
     }
-    permissionsDriver._lookup()
+    permissionsDriver._lookup();
   }
 
-  router.beforeEach((to, from, next) => {
-    if (hooks?.onBeforeEach) {
-      hooks.onBeforeEach(to, from)
+  /**
+   * Middleware Navigation Guard
+   */
+  let middlewareGuard: NavigationGuardWithThis<undefined> = (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalizedLoaded
+  ): NavigationGuardReturn => {
+    // Execute registered Pre-Hooks
+    if (options.hooks?.onBeforeEach) {
+      options.hooks.onBeforeEach(to, from);
     }
 
-    const { name, matched, meta } = to
+    //const { name, matched, meta } = to;
 
     // Add a title and id to the current page
-    if (pageTitle !== false && name) {
-      document.title = typeof pageTitle === 'object'
-        ? pageTitle.template(String(name), meta)
-        : createTitle(String(name))
+    if (options.pageTitle && to.name) {
+      if (typeof options.pageTitle === "object") {
+        document.title = options.pageTitle.template(String(to.name), to.meta);
+      }
+      document.title = createTitle(String(to.name));
     }
 
-    // Handle the role and permissions if any
+    // Handle the role and permissions with Driver if one is configured
     if (permissionsDriver) {
-      const fallbackTo = meta.fallbackTo as string | undefined || ''
-      if (permissionsDriver._hasntRole(meta)) {
-        return next({
+      const fallbackTo = to.meta.fallbackTo || "";
+      if (permissionsDriver._hasntRole(to.meta)) {
+        return {
           path: fallbackTo,
-        })
-      } else if (permissionsDriver._hasntPermissions(meta)) {
-        return next({
+        };
+      } else if (permissionsDriver._hasntPermissions(to.meta)) {
+        return {
           path: fallbackTo,
-        })
+        };
       }
     }
 
-    const redirect = (to: RouteLocationRaw) => {
-      router.push(to)
-    }
+    // const redirect = (to: RouteLocationRaw) => {
+    //   router.push(to);
+    // };
 
     const ctx: MiddlewareContext = {
       app,
       router,
       from,
       to,
-      redirect,
-      abort,
-    }
+    };
 
     // Well, looks like we don't have any middleware to run, so we can call
     // next now, otherwise we will ensure that each middleware doesn't return
     // failure by returning explicit `false` or `ABORT_KEY`..
-    const middlewaresToRun = getMiddlewares(
-      matched.map(match => match.meta)
-    )
+    const middlewaresToRun: string[] = getMiddlewares(
+      to.matched.map((match) => match.meta)
+    );
+
+    // No middlewares defined on route
     if (!middlewaresToRun.length) {
-      next()
-    } else {
-      const result: boolean[] | Symbol[] | unknown[] = middlewaresToRun.map((middlewareName) => {
-        return runMiddleware(middleware, middlewareName, ctx)
-      })
-      if (result.some(
-        (value: unknown) => value === false || value == ABORT_KEY
-      )) {
-        return next(false)
+      return true;
+    }
+
+    const result: NavigationGuardReturn[] = middlewaresToRun.map(
+      (middlewareName) => {
+        return runMiddleware(options.middleware, middlewareName, ctx);
       }
-      next()
-    }
-  })
+    );
 
+    if (result.some((value: NavigationGuardReturn) => value === false)) {
+      return false;
+    }
+  };
+
+  // Add middleware Navigation Guard to Router instance
+  router.beforeEach(middlewareGuard);
+
+  // Execute registered Post-Hooks
   router.afterEach((to, from) => {
-    if (hooks?.onAfterEach) {
-      hooks.onAfterEach(to, from)
+    if (options.hooks?.onAfterEach) {
+      options.hooks.onAfterEach(to, from);
     }
-  })
+  });
 }
 
-// Create a title from a given name
+/**
+ * Create a title from a given name
+ */
 function createTitle(name: string) {
-  return (name.charAt(0).toUpperCase() + name.slice(1)).replace(/_|-/gi, ' - ')
+  return (name.charAt(0).toUpperCase() + name.slice(1)).replace(/_|-/gi, " - ");
 }
 
-// Let's run the middleware and give the
-// middleware a bunch of parameters to play around with.
+//({ctx: MiddlewareContext, guard: string}) => void
+
+/**
+ * Let's run the middleware and give the
+ * middleware a bunch of parameters to play around with.
+ *
+ * @param middlewares to be run
+ * @param name of the Middleware
+ * @param ctx
+ * @returns a {@link NavigationGuardReturn | Navigation Guard Result} of the executed Middleware
+ */
 function runMiddleware(
-  middlewares: Middleware,
+  middlewares: Middlewares,
   name: string,
   ctx: MiddlewareContext
-) {
-  const [middleware, guard]: MiddlewareName = name.split(':') as MiddlewareName
+): NavigationGuardReturn {
+  const [middleware, guard]: MiddlewareName = name.split(":") as MiddlewareName;
 
   if (!Array.prototype.hasOwnProperty.call(middlewares, middleware)) {
     throw new Error(
       `Unknown [${middleware}] middleware, did you register this middleware?`
-    )
+    );
   }
 
+  // Run specified middleware from the middleware register
   return middlewares[middleware]({
     ...ctx,
     guard,
-  })
+  });
 }
 
-function getMiddlewares(middlewares: RouteMeta[]) {
-  return middlewares.reduce((middlewares: string[], meta: RouteMeta) => {
-    if (meta.excludeMiddleware) {
-      const excludes = Array.isArray(meta.excludeMiddleware)
-        ? meta.excludeMiddleware
-        : [meta.excludeMiddleware]
-      middlewares = middlewares.filter(name => !excludes.includes(name))
-    }
+/**
+ * Get Middleware Names from route meta tags
+ *
+ * @param metas to be extracted from
+ * @returns a list of middleware names
+ */
+function getMiddlewares(metas: RouteMeta[]): string[] {
+  let middlewareNames = metas
+    .map((meta: RouteMeta): string[] => {
+      // Filter out excluded middlewares
+      if (meta.excludeMiddleware) {
+        const excludes = meta.excludeMiddleware;
+        meta.middleware = meta.middleware?.filter(
+          (name) => !excludes.includes(name)
+        );
+      }
 
-    if (Array.isArray(meta.middleware)) {
-      middlewares.push(...meta.middleware as [])
-    } else if (typeof meta.middleware === 'string') {
-      middlewares.push(meta.middleware)
-    }
+      if (meta.middleware) {
+        // meta.middleware.push(...meta.middleware);
+        return meta.middleware;
+      }
+      return [];
+    })
+    .reduce((prev: string[], curr: string[]): string[] => {
+      return prev.concat(curr);
+    });
 
-    return middlewares
-  }, [])
+  // Filter out duplicate middleware names
+  return Array.from(new Set(middlewareNames));
 }
--- packages/vue-middleware/src/index.ts
+++ packages/vue-middleware/src/index.ts
@@ -1,33 +1,40 @@
-import type { App } from 'vue'
-import type { Router } from 'vue-router'
-import { type Options, handler } from './handler'
-import './globalDeclarations'
+import type { App, Plugin } from "vue";
+import type { Router } from "vue-router";
+import { type Options, handler } from "./handler";
 
-export type { Options, MiddlewareContext } from './handler'
+export type { Options, MiddlewareContext } from "./handler";
 
-export { Driver } from './drivers/driver'
-export * from './drivers'
-export * from './composables'
+export { Driver } from "./drivers/driver";
+export * from "./drivers";
+export * from "./composables";
 
-declare module 'vue' {
+declare module "vue" {
   interface ComponentCustomProperties {
-    is: (value: string) => boolean
-    can: (value: string) => boolean
+    is: (value: string) => boolean;
+    can: (value: string) => boolean;
   }
 }
 
-const plugin = {
-  install(app: App, options: Partial<Options> = {}) {
-    const router: Router = app.config.globalProperties.$router
-
-    if (!app.config.globalProperties.$router) {
-      throw new Error(
-        'The vue-router is required in order to work with vue-middleware.'
-      )
-    }
-
-    handler(app, router, options)
-  },
+declare module "vue-router" {
+  interface RouteMeta {
+    middleware?: string[];
+    excludeMiddleware?: string[];
+    fallbackTo?: string;
+  }
 }
 
-export default plugin
+const plugin: Plugin<Options> = {
+  install(app: App, options: Options) {
+    // Check if Vue-Router is defined in App
+    const router: Router = app.config.globalProperties.$router;
+    if (!app.config.globalProperties.$router) {
+      throw new Error(
+        "The vue-router is required in order to work with vue-middleware."
+      );
+    }
+
+    handler(app, router, options);
+  },
+};
+
+export default plugin;
